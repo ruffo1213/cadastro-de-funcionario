fc5a3feab195da20ad9e6f715290d97e14a9fdf6
ALAN RUFFO <alanruffo777@gmail.com>
2024-07-23 22:07:56 -0300

    cadastro-de-funcionarios

--- src/server.js
+++ src/server.js
@@ -1,26 +1,21 @@
-var express = require('express');
-var app = express();
-
-//configurando o servidor para usar EJS
-
-app.set('view engine', 'ejs');
-
-//passando o caminho da pasta view
-
-app.set('views', 'src/views');
-
-//configurando pasta publica para arquivo estatico
-
-app.use(express.static('src/public'));
-
-//mostrando a pagina index.ejs
-
-app.get('/', function(request, response){
-    response.render('index');
+const express = require("express");
+const path = require("path");
+const enableHotReload = require("./hot-heload");
+const app = express();
+// Configurações do seu app Express
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+console.log("Views path set to:", path.join(__dirname, "views"));
+// Configuração de pasta pública
+app.use(express.static(path.join(__dirname, "public")));
+console.log("Static files path set to:", path.join(__dirname, "public"));
+// Habilitar hot-reload
+enableHotReload(app);
+// Rotas
+app.get("/", (req, res) => {
+res.render("index");
 });
-
-//iniciando o servidor
-
-app.listen(3003, function(){
-    console.log('Servidor rodando com sucesso na porta 3003');
-});
+// Inicie o servidor
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);});
